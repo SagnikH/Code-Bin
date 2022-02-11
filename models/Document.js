6b1c346cc0c8b5da5e6c92cc1c6cde8a85b3190e
Sagnik Haldar <sagnikh2@gmail.com>
2022-02-11 19:10:12 +0530

    preetier formating

--- models/Document.js
+++ models/Document.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const DocumentSchema = new Schema({
-    text : {
-        type : String,
-        required : true
-    }
-})
+	text: {
+		type: String,
+		required: true,
+	},
+});
 
 module.exports = mongoose.model("Document", DocumentSchema);
-
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require("dotenv").config()
+require("dotenv").config();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({ extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Document = require("./models/Document");
 // const URI = `mongodb+srv://admin:${process.env.PASSWORD}@dip-bin.ysmvr.mongodb.net/Randomtext?retryWrites=true&w=majority`;
@@ -13,72 +13,61 @@ const URI = process.env.MONGODB_URI;
 
 app.listen(process.env.PORT);
 
-console.log("CHECKKKK!!!!!!!");
-
 (async () => {
-
-    try{
-        const connection = await mongoose.connect(URI, {
-                            useNewUrlParser: true,
-                            useUnifiedTopology: true
-                        });
-
-        console.log(connection);
-    } catch(e) {
-        console.log(e);
-    }
-    
+	try {
+		const connection = await mongoose.connect(URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+	} catch (e) {
+		console.log(e);
+	}
 })();
 
-// mongoose.connect(URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-
-
-app.get('/', (req, res) => {
-    const code  = `Sharing code is a good thing, and it should be _really_ easy to do it.
+app.get("/", (req, res) => {
+	const code = `Sharing code is a good thing, and it should be _really_ easy to do it.
 A lot of times, I want to show you something I'm seeing - and that's where we
 use pastebins.Haste is the prettiest, easiest to use pastebin ever made`;
 
-    res.render("code_window", {code, language : "plaintext"});
+	res.render("code_window", { code, language: "plaintext" });
 });
 
-app.get('/new', (req, res) => {
-    res.render("new");
+app.get("/new", (req, res) => {
+	res.render("new");
 });
 
-app.post('/save', async (req, res) => {
-    const value = req.body.value;
-    try{
-        const document = await Document.create({ text : value });
-        const id = document._id.toString();
-        res.redirect(`/${id}`);
-        // console.log(document._id.toString());
-    } catch(e){
-        console.log(e);
-        res.render("new", { value });
-    }
+app.post("/save", async (req, res) => {
+	const value = req.body.value;
+	try {
+		//create() is a static function of the Model class
+		const document = await Document.create({ text: value });
+		const id = document._id.toString();
+		res.redirect(`/${id}`);
+		// console.log(document._id.toString());
+	} catch (e) {
+		console.log(e);
+		res.render("new", { value });
+	}
 });
 
-app.get('/:id', async (req, res) => {
-    const id = req.params.id;
+app.get("/:id", async (req, res) => {
+	const id = req.params.id;
 
-    try{
-        const doc = await Document.findById(id);
-        res.render("code_window", { code : doc.text, id });
-    } catch(e) {
-        res.redirect("/");
-    }
+	try {
+		const doc = await Document.findById(id);
+		res.render("code_window", { code: doc.text, id });
+	} catch (e) {
+		res.redirect("/");
+	}
 });
 
-app.get('/:id/duplicate', async (req, res) => {
-    const id = req.params.id;
+app.get("/:id/duplicate", async (req, res) => {
+	const id = req.params.id;
 
-    try{
-        const doc = await Document.findById(id);
-        res.render("new", { value : doc.text });
-    } catch(e) {
-        res.redirect("/");
-    }
+	try {
+		const doc = await Document.findById(id);
+		res.render("new", { value: doc.text });
+	} catch (e) {
+		res.redirect("/");
+	}
 });
